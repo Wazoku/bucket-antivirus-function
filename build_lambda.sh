d06f2358d4e55359206fba36ef17407410428389
Chris Bailey <chris.p.bailey@gmail.com>
2022-10-18 17:22:01 +0100

    Using lambda p3.9 base image

--- build_lambda.sh
+++ build_lambda.sh
@@ -18,29 +18,31 @@
 set -e
 
 yum update -y
-yum install -y cpio python3-pip zip git json-c
+yum install -y cpio zip git json-c
 pip install --no-cache-dir virtualenv
-python3.9 -m venv  env
-. env/bin/activate
+virtualenv /tmp/env
+. /tmp/env/bin/activate
 pip install --no-cache-dir -r requirements.txt
 
+git config --global --add safe.directory /opt/app
 lambda_output_file=/opt/app/build/lambda_$(git rev-parse --short HEAD).zip
 
 pushd /tmp
-yumdownloader -x \*i686 --archlist=x86_64 clamav clamav-lib clamav-update json-c  libtool-ltdl.x86_64 libxml2.x86_64 xz-libs.x86_64
-rpm2cpio clamav-0*.rpm | cpio -idmv
-rpm2cpio clamav-lib*.rpm | cpio -idmv
-rpm2cpio clamav-update*.rpm | cpio -idmv
-rpm2cpio json*.rpm | cpio -idmv
-rpm2cpio libt*.rpm | cpio -idmv
-rpm2cpio libxml*.rpm | cpio -idmv
-rpm2cpio xz*.rpm | cpio -idmv
+yum install -y clamav-server \
+    clamav-data \
+    clamav-update \
+    clamav-filesystem \
+    clamav \
+    clamav-devel \
+    clamav-lib
 popd
-mkdir -p bin
-cp /tmp/usr/bin/clamscan /tmp/usr/bin/freshclam /tmp/usr/lib64/* bin/.
-echo "DatabaseMirror database.clamav.net" > bin/freshclam.conf
+mkdir -p /tmp/bin
+cp /usr/bin/clamscan /usr/bin/freshclam /tmp/bin/.
+ldd /usr/bin/clamscan | grep "\/.*\.so[^ ]*" -o | xargs -I{} cp {} /tmp/bin/.
+echo "DatabaseMirror database.clamav.net" > /tmp/bin/freshclam.conf
 
-mkdir -p build
-zip -r9 $lambda_output_file *.py bin
-cd env/lib/python3.9/site-packages
+zip -r9 $lambda_output_file *.py
+cd /tmp/
+zip -r9 $lambda_output_file bin
+cd /tmp/env/lib/python3.9/site-packages
 zip -r9 $lambda_output_file *
